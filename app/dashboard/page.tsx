561f64df7bb0a9d7b33e12ff0a42e1110ea349a3
Alex Lee <alexlee@Alexs-MacBook-Air.local>
2026-04-06 19:08:01 -0400

    added sign up route and page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,14 +1,6 @@
-import React from 'react'
-import { getCurrentUser } from '../lib/auth'
-import { useRouter } from 'next/navigation';
 
 const DashboardPage = async () => {
-  const user = await getCurrentUser();
-  const router = useRouter();
 
-  if (!user) {
-    router.push("/sign-up")
-  }
   
   return (
     <div>DashboardPage</div>
--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -10,7 +10,10 @@ const SignInPage = () => {
   });
 
   const [serverMessage, setServerMessage] = useState("");
-  const [fieldError, setFieldError] = useState({});
+  const [fieldError, setFieldError] = useState<{
+    email?: string;
+    password?: string;
+  }>({});
   const router = useRouter();
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -50,27 +53,57 @@ const SignInPage = () => {
   return (
     // Outer Container
     <div className="min-h-screen bg-zinc-200">
-        {/* Inner Container */}
-        <div className="p-10 md:py-14">
-            {/* Form width */}
-            <div className="max-w-md mx-auto">
-                <form 
-                onSubmit={handleSubmit} 
-                className="bg-white shadow-md p-5 rounded-lg"
-                >
-                    {/* Header */}
-                    <div>
-                        <h2 className="font-bold text-zinc-700 text-center">
-                            Sign In
-                        </h2>
-                    </div>
-                    <div className="">
-
-                    </div>
-                </form>
+      {/* Inner Container */}
+      <div className="p-10 md:py-14">
+        {/* Form width */}
+        <div className="max-w-md mx-auto">
+          <form
+            onSubmit={handleSubmit}
+            className="bg-white shadow-md p-5 rounded-lg"
+          >
+            {/* Header */}
+            <div>
+              <h2 className="font-bold text-zinc-700 text-center pb-5">
+                Sign In
+              </h2>
             </div>
-
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+              <div className="space-y-2">
+                <label className="text-zinc-500 text-sm block">Email</label>
+                <input
+                  name="email"
+                  type="email"
+                  onChange={onChange}
+                  value={form.email}
+                  className="block outline outline-zinc-200 rounded-md focus:ring-2 focus:outline-none focus:ring-blue-500 w-full"
+                />
+                {fieldError.email && (
+                  <p className="text-sm text-red-500">{fieldError.email}</p>
+                )}
+              </div>
+              <div className="space-y-2">
+                <label className="text-zinc-500 text-sm block">Password</label>
+                <input
+                  name="password"
+                  type="password"
+                  onChange={onChange}
+                  value={form.password}
+                  className="block outline outline-zinc-200 rounded-md focus:ring-2 focus:outline-none focus:ring-blue-500 w-full"
+                />
+                {fieldError.password && (
+                  <p className="text-sm text-red-500">{fieldError.password}</p>
+                )}
+              </div>
+            </div>
+            <div className="py-4"></div>
+            <button 
+            type="submit"
+            className="block mx-auto w-fit p-2 px-3 bg-blue-600 text-gray-200 rounded-lg"
+            >
+              Sign In</button>
+          </form>
         </div>
+      </div>
     </div>
   );
 };
